c5ed850c25f0f096ecb8914e793d648f1dc678ea
Eli T. Drumm <etd@dte.li>
2019-12-03 11:20:40 -0500

    simple routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MydrinksComponent } from './mydrinks/mydrinks.component';
+import { MixComponent } from './mix/mix.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'drinks', component: MydrinksComponent },
+  { path: 'mix', component: MixComponent },
+  { path: 'search', component: SearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -6,12 +6,12 @@ import { QIngredient, Drink, Ingredient, CDBDrink, CDBDrinksObject } from './typ
 
 const BASEURL = "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?";
 
-const cDBIngFields = ['strIngredient1','strIngredient2','strIngredient3','strIngredient4','strIngredient5',
-                      'strIngredient6','strIngredient7','strIngredient8','strIngredient9','strIngredient10',
-                      'strIngredient11','strIngredient12','strIngredient13','strIngredient14','strIngredient15']
-const cDBMeasureFields = ['strMeasure1','strMeasure2','strMeasure3','strMeasure4','strMeasure5',
-                          'strMeasure6','strMeasure7','strMeasure8','strMeasure9','strMeasure10',
-                          'strMeasure11','strMeasure12','strMeasure13','strMeasure14','strMeasure15']
+// const cDBIngFields = ['strIngredient1','strIngredient2','strIngredient3','strIngredient4','strIngredient5',
+//                       'strIngredient6','strIngredient7','strIngredient8','strIngredient9','strIngredient10',
+//                       'strIngredient11','strIngredient12','strIngredient13','strIngredient14','strIngredient15']
+// const cDBMeasureFields = ['strMeasure1','strMeasure2','strMeasure3','strMeasure4','strMeasure5',
+//                           'strMeasure6','strMeasure7','strMeasure8','strMeasure9','strMeasure10',
+//                           'strMeasure11','strMeasure12','strMeasure13','strMeasure14','strMeasure15']
 
 @Injectable({
   providedIn: 'root'
@@ -39,7 +39,7 @@ export class SearchService {
           d.name = cdo.strDrink;
           d.instructions = cdo.strInstructions;
           for (let i = 0; i < 15; i++) {
-            if (d['strIngredient'+(i+1).toString()]) {
+            if (cdo['strIngredient'+(i+1).toString()]) {
               d.ingredients.push({
                 amount: cdo['strMeasure'+(i+1).toString()],
                 name: cdo['strIngredient'+(i+1).toString()],
