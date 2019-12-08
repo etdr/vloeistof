cec2b7273f9da94c40df251ce8d680c8a2c03a23
Reese Dick <reeserdick@gmail.com>
2019-12-08 16:39:46 -0500

    copying

--- src/app/mix/mix.component.ts
+++ src/app/mix/mix.component.ts
@@ -15,71 +15,119 @@ const MEASUREMENTS = ['oz ', 'ozs ', 'ounces ', 'part ', 'parts ', 'cl ', 'cls '
   templateUrl: './mix.component.html',
   styleUrls: ['./mix.component.scss']
 })
-export class MixComponent {
+// export class MixComponent {
 
-  drinkName: string = "";
-  instructions: string = "";
+//   drinkName: string = "";
+//   instructions: string = "";
 
+//   visible = true;
+//   selectable = true;
+//   removable = true;
+//   addOnBlur = true;
+//   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+//   ings: QIngredient[] = [];
+
+//   constructor (private drinksService: DrinksService) { }
+
+
+//   splitIngredient (qstr: string): string[] {
+//     let a = ''; let i = '';
+//     for (let m of MEASUREMENTS) {
+//       if (qstr.indexOf(m) >= 0) {
+//         let spl = qstr.indexOf(m);
+//         a = qstr.slice(0, spl + m.length);
+//         i = qstr.slice(spl + m.length );
+//       }
+//     }
+//     if (i.indexOf("of ") >= 0) i = i.split("of ")[1];
+//     console.log('amount', a, '; ing', i);
+//     return [a, i];
+//   }
+
+
+
+//   add(event: MatChipInputEvent): void {
+//     const input = event.input;
+//     const value = event.value;
+
+//     const [a, i] = this.splitIngredient(value);
+
+//     if ((value || '').trim()) {
+//       this.ings.push({name: i,
+//         amount: a,
+//         id: 0
+//       });
+//     }
+//     if (input) {
+//       input.value = '';
+//     }
+//   }
+//   remove(i: QIngredient): void {
+//     const index = this.ings.indexOf(i);
+
+//     if (index >= 0) {
+//       this.ings.splice(index, 1);
+//     }
+//   }
+
+
+//   addDrink () {
+//     this.drinksService.addDrink({
+//       name: this.drinkName,
+//       id: 0,
+//       ingredients: this.ings,
+//       instructions: this.instructions,
+//       thumbUrl: '',
+//       userId: 0,
+//       cDBId: 0
+//     }).subscribe(res => console.log(res));
+//   }
+// } 
+
+
+export class MixComponent implements OnInit {
   visible = true;
   selectable = true;
   removable = true;
   addOnBlur = true;
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
-  ings: QIngredient[] = [];
+  ingredients: QIngredient[] = [];
 
-  constructor (private drinksService: DrinksService) { }
+  inputAmount: string = "";
+  inputIngredient: string = "";
 
 
-  splitIngredient (qstr: string): string[] {
-    let a = ''; let i = '';
-    for (let m of MEASUREMENTS) {
-      if (qstr.indexOf(m) >= 0) {
-        let spl = qstr.indexOf(m);
-        a = qstr.slice(0, spl + m.length);
-        i = qstr.slice(spl + m.length );
-      }
-    }
-    if (i.indexOf("of ") >= 0) i = i.split("of ")[1];
-    console.log('amount', a, '; ing', i);
-    return [a, i];
+  constructor() { }
+
+  ngOnInit() {
   }
-
 
 
   add(event: MatChipInputEvent): void {
     const input = event.input;
     const value = event.value;
 
-    const [a, i] = this.splitIngredient(value);
-
     if ((value || '').trim()) {
-      this.ings.push({name: i,
-        amount: a,
-        id: 0
-      });
+      this.ingredients.push({
+        id: 0,
+        amount: this.inputAmount,
+        name: this.inputIngredient
+      })
     }
+
     if (input) {
-      input.value = '';
+      input.value='';
+      this.inputAmount= '';
     }
   }
-  remove(i: QIngredient): void {
-    const index = this.ings.indexOf(i);
-
+  
+  remove(ingredient: QIngredient): void {
+    const index = this.ingredients.indexOf(ingredient);
+    
     if (index >= 0) {
-      this.ings.splice(index, 1);
+      this.ingredients.splice(index, 1);
     }
+
   }
 
-
-  addDrink () {
-    this.drinksService.addDrink({
-      name: this.drinkName,
-      id: 0,
-      ingredients: this.ings,
-      instructions: this.instructions,
-      thumbUrl: '',
-      userId: 0,
-      cDBId: 0
-    }).subscribe(res => console.log(res));
-  }
-} 
+}
