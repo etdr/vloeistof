a700773763dce4194cebc04bc9cefdf162fe4306
Eli T. Drumm <etd@dte.li>
2019-12-18 08:25:43 -0500

    edit app.js

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('./dist/vloeistof'));
+//app.server.get('*.*', express.static('dist'))
 
-app.all('*', (req, res) => {
-  res.sendFile('/', {root: 'dist'});
+app.get('*', (req, res) => {
+  res.status(200).sendFile('index.html', {root: 'dist/vloeistof'});
 });
 
 
-app.listen(() => console.log(process.env.PORT || 3025, "listening on port"));
+app.listen(3025, () => console.log("listening on port"));
